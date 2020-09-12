3dd9790cd0d56648638db8fbaacf8072ffff0c96
Fbessb <francois.bessaguet@gmail.com>
2020-09-12 13:53:25 +0200

    Updates
    Some updates

--- src/closure_two/core.clj
+++ src/closure_two/core.clj
@@ -1,7 +1,17 @@
 (ns closure-two.core
   (:gen-class))
+(declare test-args)
 
 (defn -main
-  "I don't do a whole lot ... yet."
+ "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (println "Hello, World! At last II")
+  (println (test-args (first args)))
+)
+
+(defn test-args [value]
+  (if (= value "Zoom") 
+      (println "Zoom Ok")
+      (println "Zoom Nok")
+  )
+)
